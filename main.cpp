92ecb3a4daef91e850daf8ac901a153a485085d3
TbYangZ <1104524667@qq.com>
2023-04-24 13:19:47 +0800

    4-24

--- main.cpp
+++ main.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 #include "tensor.h"
 int main() {
-<<<<<<< HEAD
-    SimpleTensor::Tensor tensor;
-=======
-    SimpleTensor::Tensor tensor = SimpleTensor::Tensor::randn({2, 2, 3});
-    
->>>>>>> 3faff32e682e9843223f7fdc0c222bcaf7e49bb3
     return 0;
 }
